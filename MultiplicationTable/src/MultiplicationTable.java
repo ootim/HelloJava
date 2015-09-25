2fabbf262d2be60af4b51d717d6911395a592cc0
Tim <o.oladoyinbo@gmail.com>
2015-09-25 09:12:53 +0100

    update to multiplication table

---
+++ MultiplicationTable/src/MultiplicationTable.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Timileyin on 25/09/2015.
+ */
+import java.util.Scanner;
+
+public class MultiplicationTable {
+
+    public static void main(String args[])
+    {
+        int start, end, i, val;
+        Scanner mScanner = new Scanner(System.in);
+        System.out.println("Enter range of numbers to print multiplication table:");
+        start = mScanner.nextInt();
+        end = mScanner.nextInt();
+
+        for(i=start; i<=end; i++)
+        {
+            for(val=1; val<=10; val++)
+            {
+                System.out.print(i * val + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+}
